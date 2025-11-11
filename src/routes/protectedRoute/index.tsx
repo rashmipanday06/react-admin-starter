c9874fd4346847f125e925fb4a08d740d58dbabd
rashmipanday06 <rashmipanday06598@gmail.com>
2025-11-12 00:27:10 +0530

    protected route is created.

---
+++ src/routes/protectedRoute/index.tsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../componetns/context/authContext";
+import type { ReactNode } from "react";
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+  const { token } = useAuth();
+
+  if (!token) return <Navigate to="/login" replace />;
+
+  return children;
+}
